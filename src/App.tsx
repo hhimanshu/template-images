b566c89e3507eca51c1217b177abf62b3c75e53f
hhimanshu <dev.atwork@yahoo.com>
2023-06-09 17:15:25 -0700

    feat(): ability to create image of high quality using pixel ratio

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 import {Card, Flex, Metric, ProgressBar, Text} from "@tremor/react";
+import {toJpeg} from "html-to-image";
+import {useCallback, useRef} from "react";
 
 function App() {
+    const ref = useRef<HTMLDivElement>(null)
+    const onButtonClick = useCallback(() => {
+        if (ref.current === null) {
+            return
+        }
+
+        toJpeg(ref.current, {cacheBust: true, pixelRatio: 50})
+            .then((dataUrl) => {
+                const link = document.createElement('a')
+                link.download = 'my-image-name.png'
+                link.href = dataUrl
+                link.click()
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [ref])
     return (
         <>
-            <Card className="max-w-xs mx-auto">
+            <Card className="max-w-xs" ref={ref}>
                 <Text>Sales</Text>
                 <Metric>$ 71,465</Metric>
                 <Flex className="mt-4">
                     <Text>32% of annual target</Text>
                     <Text>$ 225,000</Text>
                 </Flex>
-                <ProgressBar value={32} className="mt-2" />
+                <ProgressBar value={32} className="mt-2"/>
             </Card>
+            <button onClick={onButtonClick}>Download</button>
         </>
     )
 }
